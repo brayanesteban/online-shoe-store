34a3f2c6519aeb2476cc7759ea6217289ade5f7e
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-03-10 20:16:04 -0500

    Primer version de la pagina web

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarQR() {
+
+let qr = document.getElementById("qr-nequi");
+
+if(qr.style.display === "block"){
+qr.style.display = "none";
+}else{
+qr.style.display = "block";
+}
+
+}
