562df0804bf4dead4cb665913bb1b5f44ea15329
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-03-11 13:04:50 -0500

    changes of index and catalogo with animation in catalogo

--- Html/script.js
+++ Html/script.js
@@ -9,3 +9,26 @@ qr.style.display = "block";
 }
 
 }
+
+const catalogo = document.getElementById("catalogoBtn");
+
+catalogo.addEventListener("mouseover", function(){
+
+for(let i=0;i<3;i++){
+
+let burbuja = document.createElement("span");
+
+burbuja.classList.add("burbuja");
+
+burbuja.style.left = Math.random()*100 + "%";
+burbuja.style.top = Math.random()*40 + "px";
+
+catalogo.appendChild(burbuja);
+
+setTimeout(()=>{
+burbuja.remove();
+},1000);
+
+}
+
+});
