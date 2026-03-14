925bf182bd98f85f92f4a1b39ac29534817a9197
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-03-14 16:21:49 -0500

    complete index

--- Html/script.js
+++ Html/script.js
@@ -1,3 +1,4 @@
+aler("js cargado");
 function mostrarQRNequi() {
   var modal = document.getElementById("modal-qr-nequi");
   if (modal) {
@@ -12,7 +13,6 @@ function cerrarModalQR() {
   }
 }
 
-// Cerrar modal al hacer clic fuera del contenido
 window.addEventListener('click', function(event) {
   var modal = document.getElementById("modal-qr-nequi");
   if (modal && modal.classList.contains("activo")) {
@@ -42,3 +42,93 @@ if (catalogo) {
 
 const titulo = document.getElementById('titulo-catalogo');
 titulo.textContent = 'Catálogo';
+
+function abrirFormulario(){
+
+document.getElementById("formularioCompra").style.display = "block";
+
+}
+
+function calcularDomicilio(){
+
+let domicilio = document.getElementById("zona").value;
+
+document.getElementById("precioDomicilio").innerText = domicilio;
+
+let total = 90000 + Number(domicilio);
+
+document.getElementById("precioTotal").innerText = total;
+
+}
+
+function mostrarQR(){
+
+document.getElementById("pagoQR").style.display = "block";
+
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+});
+
+
+const productos = {
+
+1:{
+nombre:"Zapatos Azules Oscuros",
+precio:90000,
+imagen:"imagenes/Zapatosazuloscuro.jfif"
+},
+
+2:{
+nombre:"Zapatos Blancos",
+precio:90000,
+imagen:"imagenes/Zapatosblancos.jfif"
+},
+
+3:{
+nombre:"Zapatos Negros",
+precio:90000,
+imagen:"imagenes/Zapatosnegros.jfif"
+},
+
+4:{
+nombre:"Zapatos Rojos",
+precio:90000,
+imagen:"imagenes/Zapatosrojos.jfif"
+},
+
+5:{
+nombre:"Zapatos Verdes",
+precio:90000,
+imagen:"imagenes/Zapatosverdes.jfif"
+}
+
+};
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("producto");
+
+const producto = productos[id];
+
+if(producto){
+
+const nombre = document.getElementById("nombreProducto");
+const imagen = document.getElementById("imagenProducto");
+const precio = document.getElementById("precioProducto");
+
+if(nombre && imagen && precio){
+
+nombre.textContent = producto.nombre;
+imagen.src = producto.imagen;
+precio.textContent = "Precio: $" + producto.precio.toLocaleString();
+
+}
+
+}
+
+
+
+
+
+
+
